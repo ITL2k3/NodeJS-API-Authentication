888e2e0008340db7bce26663fd995671273681de
ITL2k3 <91226590+ITL2k3@users.noreply.github.com>
2023-09-12 15:18:52 +0700

    #1

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const createError = require('http-errors')
+const UserRoute = require('./routes/User.route')
+const app = express()
+
+require('dotenv').config()
+
+app.get('/',(req,res,next) => {
+    res.send('Home page')
+})
+
+app.use('/user',UserRoute)
+
+app.use((req,res,next) => {
+    // const error = new Error('Not Found')
+    // error.status = 500
+    // next(error)
+    next(createError.NotFound('This route does not exists!'))
+})
+
+app.use((err,req,res,next) => {
+    res.json({
+        status: err.status || 500,
+        message: err.message
+    })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
